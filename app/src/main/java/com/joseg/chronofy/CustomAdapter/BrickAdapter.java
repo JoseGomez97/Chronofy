698a8248b5db4d67952703caf9f5a44d6d23724e
Jose Gómez <josegomezgadea@gmail.com>
2019-01-22 18:50:23 +0100

    Adaptador y modelo terminado a falta de la parte del MainActivity.java https://www.journaldev.com/10416/android-listview-with-custom-adapter-example-tutorial

---
+++ app/src/main/java/com/joseg/chronofy/CustomAdapter/BrickAdapter.java
@@ -0,0 +1,131 @@
+package com.joseg.chronofy.CustomAdapter;
+
+/*
+    Se encarga de adaptar el modelo del brick a la ListView
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.joseg.chronofy.DataModel.BrickModel;
+import com.joseg.chronofy.R;
+
+import java.util.ArrayList;
+
+public class BrickAdapter extends ArrayAdapter<BrickModel> implements View.OnClickListener{
+
+    private ArrayList<BrickModel> dataSet;
+    Context mContext;
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView tituloBrick;
+        TextView descripcionBrick;
+        ImageView imagenBrick;
+        ImageView imagenOpcionesBrick;
+    }
+
+    public BrickAdapter(ArrayList<BrickModel> data, Context context) {
+        super(context, R.layout.brick_view, data);
+        this.dataSet = data;
+        this.mContext=context;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        final int position=(Integer) v.getTag();
+        Object object= getItem(position);
+        BrickModel brickModel=(BrickModel) object;
+
+        switch (v.getId())
+        {
+            case R.id.imagen_opciones_brick:
+                // Cuando pulsemos sobre las opciones del brick...
+                PopupMenu popupMenu = new PopupMenu(getContext(), v);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        // TODO Acción de cada uno de los botones.
+                        switch (item.getItemId()) {
+                            case R.id.reiniciar_brick:
+                                Toast.makeText(getContext(), "Reiniciar",
+                                        Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.editar_brick:
+                                Toast.makeText(getContext(), "Editar",
+                                        Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.eliminar_brick:
+                                dataSet.remove(position);
+                                Toast.makeText(getContext(), "Elemento " + position + " eliminado",
+                                        Toast.LENGTH_SHORT).show();
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.inflate(R.menu.brick);
+                popupMenu.show();
+                break;
+
+            default:
+                // Cuando pulsemos sobre un brick...
+                Snackbar.make(v, "Brick pulsado", Snackbar.LENGTH_LONG)
+                        .setAction("No action", null).show();
+                break;
+        }
+    }
+
+    private int lastPosition = -1;
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        BrickModel brickModel = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
+        final View result;
+
+        if (convertView == null) {
+
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.brick_view, parent, false);
+            viewHolder.tituloBrick = convertView.findViewById(R.id.titulo_brick);
+            viewHolder.descripcionBrick = convertView.findViewById(R.id.descripcion_brick);
+            viewHolder.imagenBrick = convertView.findViewById(R.id.imagen_brick);
+
+            result=convertView;
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result=convertView;
+        }
+
+        Animation animation = AnimationUtils.loadAnimation(mContext, (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
+        result.startAnimation(animation);
+        lastPosition = position;
+
+        viewHolder.tituloBrick.setText(brickModel.getName());
+        viewHolder.descripcionBrick.setText(brickModel.getDescription());
+        viewHolder.imagenOpcionesBrick.setOnClickListener(this);
+        viewHolder.imagenOpcionesBrick.setTag(position);
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/joseg/chronofy/MainActivity.java
@@ -0,0 +1,117 @@
+package com.joseg.chronofy;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Pone como View a activity_main.xml
+        setContentView(R.layout.activity_main);
+
+        // El activity_main tiene una toolbar.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // El activity_main tiene un botón flotante (fab)
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // El activity_main tiene un DrawerLayout, que contiene el resto de objetos
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        // En la toolbar se pone el acceso al menú lateral que ocupará lo que ocupa el drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.setDrawerSlideAnimationEnabled(false);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        // Este es el menú lateral a desplegar
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Al botón de atrás añadimos la función de cerrar el drawer_layout en caso de estar abierto
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Añade las opciones al menú de tres puntos de la toolbar
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            // En caso de pulsar el botón de opciones...
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
